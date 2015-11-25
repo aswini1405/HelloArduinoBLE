98511a4f05f4fd9e4ae322b56b306250114c6ad9
aswini1405 <aswini@velo-labs.com>
2015-11-25 13:21:09 +0530

    push updates

--- app/src/main/java/com/velolabs/helloarduinoble/DeviceScanActivity.java
+++ app/src/main/java/com/velolabs/helloarduinoble/DeviceScanActivity.java
@@ -87,8 +87,8 @@ public class DeviceScanActivity extends ListActivity {
     protected void onResume() {
         super.onResume();
 
-        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
-        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        // Ensures Bluetooth is enabled on the device.
+        // If Bluetooth is not currently enabled,then fire an intent to display a dialog asking the user to grant permission to enable it.
         if (!mBluetoothAdapter.isEnabled()) {
             if (!mBluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
